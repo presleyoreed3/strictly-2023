3f9204dd23eb32cde346c1bc99b240bb50436373
presleyoreed3 <presleyoreed3@gmail.com>
2023-10-03 16:08:44 -0700

    bottom right

---
+++ binary-trees/bottom_right.js
@@ -0,0 +1,22 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const bottomRightValue = (root) => {
+  const queue = [root];
+  let current = null;
+  while (queue.length > 0){
+    current = queue.shift();
+    if (current.left !== null) queue.push(current.left);
+    if (current.right !== null) queue.push(current.right);
+  }
+  return current.val;
+};
+
+module.exports = {
+  bottomRightValue,
+};
