eaca0b43a4ca3ff7b70412caae566eb23b585d3b
presleyoreed3 <presleyoreed3@gmail.com>
2023-10-05 10:56:40 -0700

    tree levels

---
+++ binary-trees/levels.js
@@ -0,0 +1,31 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const treeLevels = (root) => {
+  if (root === null) return [];
+  
+  const levels = [];
+  const queue = [{ node: root, levelNum: 0}];
+  while(queue.length > 0){
+    const {node, levelNum} = queue.shift();
+    
+    if (levels.length === levelNum) {
+      levels[levelNum] = [node.val];
+    } else {
+      levels[levelNum].push(node.val);
+    }
+    
+    if (node.left !== null) queue.push({node: node.left, levelNum:  levelNum + 1})
+    if (node.right !== null) queue.push({node: node.right, levelNum: levelNum + 1})
+  }
+  return levels;
+};
+
+module.exports = {
+  treeLevels,
+};
