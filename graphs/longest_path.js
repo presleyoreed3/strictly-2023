2042da3eea24309d78f3132a4c156b2a02989bb5
presleyoreed3 <presleyoreed3@gmail.com>
2023-10-16 09:53:21 -0700

    longest path

---
+++ graphs/longest_path.js
@@ -0,0 +1,33 @@
+const longestPath = (graph) => {
+  const distance = {};
+  
+  for (let node in graph){
+    if (graph[node].length === 0){
+      distance[node] = 0;
+    }
+  }
+
+  for (let node in graph) {
+    traverseDistance(graph, node, distance)
+  }
+  
+  return Math.max(...Object.values(distance));
+};
+
+const traverseDistance = (graph, node, distance) => {
+  if (node in distance) return distance[node];
+  
+  let maxLength = 0;
+  
+  for (let neighbor of graph[node]) {
+    const attempt = traverseDistance(graph, neighbor, distance);
+    if (attempt > maxLength) maxLength = attempt;
+  }
+  
+  distance[node] = 1 + maxLength;
+  return distance[node];
+}
+
+module.exports = {
+  longestPath,
+};
