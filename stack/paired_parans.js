5cd224e86884f91ad2d7d9056634524c880fac2a
presleyoreed3 <presleyoreed3@gmail.com>
2023-11-08 10:40:52 -0800

    start of stacks

---
+++ stack/paired_parans.js
@@ -0,0 +1,18 @@
+const pairedParentheses = (str) => {
+  let count = 0;
+  for (let char of str){
+    if (char === '('){
+      count += 1;
+    } else if (char === ')') {
+      if (count === 0){
+        return false;
+      }
+      count -= 1;
+    }
+  }
+  return count === 0;
+};
+
+module.exports = {
+  pairedParentheses,
+};
