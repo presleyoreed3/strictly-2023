26e576185c12db63ccf37a39db6549495680421f
presleyoreed3 <presleyoreed3@gmail.com>
2023-11-09 10:41:16 -0800

    more brackets

---
+++ stack/befitting_brackets.js
@@ -0,0 +1,23 @@
+const befittingBrackets = (str) => {
+  const stack = [];
+  
+  const brackets = {
+    '(' : ')',
+    '[' : ']',
+    '{' : '}'
+  }
+  
+  for (let char of str){
+    if (char in brackets){
+      stack.push(brackets[char]);
+    } else {
+      if (stack.length > 0 && stack[stack.length - 1] === char){
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
+};
